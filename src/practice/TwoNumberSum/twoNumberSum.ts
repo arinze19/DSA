260f911d85b633c0966b0022200d2e0e86066716
Arinze <arinze19@outlook.com>
2022-05-14 14:13:38 +0100

    modify practice folder

--- src/practice/TwoNumberSum/twoNumberSum.ts
+++ src/practice/TwoNumberSum/twoNumberSum.ts
@@ -21,6 +21,12 @@
 
 export const twoNumberSum = (array: number[], targetSum: number):number[] => {
   // write code here
-
-  return [];
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+        if (array[i] + array[j] === targetSum) {
+            return [array[i], array[j]];
+        }
+    }
+}
+return [];
 };
