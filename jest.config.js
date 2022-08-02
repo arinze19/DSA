95935fddf91439a797d0925485b5c1460ddbcfbf
Arinze <mac@Arinzes-MacBook-Pro.local>
2022-08-02 12:22:15 +0100

    changes

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,5 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-    "testMatch": [
-      "**/__tests__/**/*.+(ts|tsx|js)",
-      "**/?(*.)+(spec|test).+(ts|tsx|js)"
-    ],
-    "transform": {
-      "^.+\\.(ts|tsx)$": "ts-jest"
-    },
-  }
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
