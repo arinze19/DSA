8c043ba67f0fb3afc1f7799dfcfcb81b9d6f9f2f
Arinze <mac@Arinzes-MacBook-Pro.local>
2022-08-29 17:27:13 +0100

    add sales path

---
+++ src/practice/SalesPath/SalesPath.test.ts
@@ -0,0 +1,20 @@
+import { getCheapestCost, Node } from './SalesPath';
+
+test('Determine if sales path is shortest', () => {
+  class SalesNode  {
+    cost: number;
+    children: SalesNode[];
+    
+    constructor(cost: number, children: Node[]) {
+      this.cost = cost;
+      this.children = children;
+    }
+  }
+
+  const node4 = new SalesNode(9, []);
+  const node2 = new SalesNode(4, [node4]);
+  const node3 = new SalesNode(5, []);
+  const node1 = new SalesNode(0, [node2, node3]);
+
+  console.log(getCheapestCost(node1));
+});
