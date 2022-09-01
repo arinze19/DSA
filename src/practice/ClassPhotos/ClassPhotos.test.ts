226cc7803d39b893fe6b5f59e5f0d844e4f163ec
Arinze <mac@Arinzes-MacBook-Pro.local>
2022-09-01 18:22:55 +0100

    refactor class photos

--- src/practice/ClassPhotos/ClassPhotos.test.ts
+++ src/practice/ClassPhotos/ClassPhotos.test.ts
@@ -2,8 +2,9 @@ import { classPhotos } from './ClassPhotos';
 
 it('correctly determines if a class photo is in sequence', () => {
     expect(classPhotos([6, 9, 2, 4, 5], [5, 8, 1, 3, 4])).toBeTruthy()
-    expect(classPhotos([5, 6, 7, 2, 3, 1, 2, 3], [1, 1, 1, 1, 1, 1, 1, 1])).toBeFalsy()
     expect(classPhotos([6], [6])).toBeFalsy()
     expect(classPhotos([5, 4], [4, 5])).toBeFalsy()
     expect(classPhotos([5, 4], [5, 6])).toBeTruthy()
+    expect(classPhotos([6], [6])).toBeFalsy()
+    expect(classPhotos([126], [125])).toBeTruthy()
 })
