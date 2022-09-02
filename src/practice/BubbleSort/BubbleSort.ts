de092668770b50a0023e152a4c9ab859a9c5b026
Arinze <mac@Arinzes-MacBook-Pro.local>
2022-09-02 13:02:26 +0100

    add bubble sort

---
+++ src/practice/BubbleSort/BubbleSort.ts
@@ -0,0 +1,23 @@
+/**
+ * Write a bubble sort function
+ */
+
+export const bubbleSort = (array: number[]) => {
+  let isSorted = false;
+
+  do {
+    isSorted = true;
+
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > array[i + 1]) {
+        isSorted = false;
+        let temp = array[i];
+
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+      }
+    }
+  } while (!isSorted);
+
+  return array;
+};
