4083bd47a6d5757af6bab4e0a687024c9f51d976
Arinze <mac@Arinzes-MacBook-Pro.local>
2022-09-03 18:28:35 +0100

    add insertion sort

---
+++ src/practice/InsertionSort/InsertionSort.ts
@@ -0,0 +1,22 @@
+/**
+ * Impliment Insertion Sort
+ *
+ * RUNTIME AND SPACE COMPLEXITY
+ * O(n) time | O(1) space
+ */
+
+export const insertionSort = (array: number[]) => {
+  for (let i = 1; i < array.length; i++) {
+    const sub = array.slice(0, i);
+    for (let j = 0; j < sub.length; j++) {
+      if (array[i] < sub[j]) {
+        let temp = array[i];
+
+        array[i] = sub[j];
+        array[j] = temp;
+      }
+    }
+  }
+
+  return array;
+};
