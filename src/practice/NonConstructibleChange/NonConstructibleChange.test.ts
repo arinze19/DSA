4e934c7c1090f4cc575e20ae60ab62089dd30a95
Arinze <mac@Arinzes-MacBook-Pro.local>
2022-09-05 08:06:28 +0100

    scaffhold

---
+++ src/practice/NonConstructibleChange/NonConstructibleChange.test.ts
@@ -0,0 +1,10 @@
+export const nonConstructibleChange = (coins: number[]): number => {
+  const arr = []
+  let minimumChange = 1;
+
+  for(let i = 0; i < coins.length; i++) {
+    
+  }
+
+  return minimumChange;
+};
