11dd95ce67922e7cb37c41abe321375c6038da72
Arinze <mac@Arinzes-MacBook-Pro.local>
2022-09-22 10:20:04 +0100

    add second solution for twonumbersum

--- src/practice/TwoNumberSum/twoNumberSum.test.ts
+++ src/practice/TwoNumberSum/twoNumberSum.test.ts
@@ -1,4 +1,4 @@
-import { twoNumberSum } from './twoNumberSum';
+import { twoNumberSum, twoNumberSum2 } from './twoNumberSum';
 
 test('Determine If array contains elements that add up to target sum', () => {
     expect(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10)).toEqual([11, -1])
@@ -7,4 +7,10 @@ test('Determine If array contains elements that add up to target sum', () => {
     expect(twoNumberSum([14], 10)).toEqual([])
     expect(twoNumberSum([15], 15)).toEqual([])
     expect(twoNumberSum([4, 6, 1, -3], 3)).toEqual([6, -3])
+    expect(twoNumberSum2([3, 5, -4, 8, 11, 1, -1, 6], 10)).toEqual([11, -1])
+    expect(twoNumberSum2([4, 6], 10)).toEqual([4, 6])
+    expect(twoNumberSum2([3, 5, -4, 8, 11, 1, -1, 6], 15)).toEqual([])
+    expect(twoNumberSum2([14], 10)).toEqual([])
+    expect(twoNumberSum2([15], 15)).toEqual([])
+    expect(twoNumberSum2([4, 6, 1, -3], 3)).toEqual([6, -3])
 })
