1022ed3c91a55215967d6cadb1aec73d06e33b1b
Arinze Obi <arinze19@outlook.com>
2023-02-24 22:59:56 +0100

    .

--- src/curriculum/two-pointers/TwoSum2/TwoSum.cpp
+++ src/curriculum/two-pointers/TwoSum2/TwoSum.cpp
@@ -2,15 +2,24 @@
 #include <map>
 #include <vector>
 
-std::vector<int> twoSum(std::vector<int> nums, int target)
+int twoSum(std::vector<int> nums, int target)
 {
     std::map<int, int> map;
 
-    for (int index = 0; index < nums.size(); index++)
+    for (int i = 0; i < nums.size(); i++)
     {
+        map.insert(target - nums[i], i + 1);
+        std::cout << map[i] << "\n";
+        // std::map<int, int>::iterator idx1 = map.find(target - nums[i]);
     }
+
+    return 0;
 }
 
 int main()
 {
+    std::vector<int> numbers;
+    numbers[0] = 1;
+    numbers[1] = 2;
+    twoSum(numbers, 9);
 }
